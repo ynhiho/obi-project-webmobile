2c4b9a481ec55f65dcedbd169ec8c73b9b4ce547
Yen Nhi Ho <yen.nhi.ho@iem.thm.de>
2020-03-10 19:30:44 +0100

    crawling: subcategories fetched

--- crawler.js
+++ crawler.js
@@ -1,4 +1,5 @@
 var Crawler = require("crawler");
+let categories = [];
 
 var c = new Crawler({
   maxConnections: 10,
@@ -7,13 +8,15 @@ var c = new Crawler({
     if (error) {
       console.log(error);
     } else {
-      let categories = [];
       var $ = res.$;
 
       categories = setCategories($, categories);
 
-      console.log(categories);
-      console.log(categories.length);
+      // console.log(categories);
+      // console.log(categories.length);
+      categories.map(category => {
+        c2.queue(category.url);
+      });
     }
     done();
   }
@@ -56,3 +59,38 @@ function setCategories($, categories) {
 
 // Queue a list of URLs
 c.queue(["https://www.obi.de/"]);
+
+var c2 = new Crawler({
+  maxConnections: 10,
+  // This will be called for each crawled page
+  callback: function(error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      var $ = res.$;
+      let subcategories = { parentCategory: "", subcats: [] };
+
+      subcategories.subcats = setSubCategories($);
+      subcategories.parentCategory = getParentCategory($);
+
+      console.log(subcategories);
+    }
+    done();
+  }
+});
+
+function setSubCategories($) {
+  let helper = [];
+  $(".first-level li a").each(function(i, element) {
+    const $element = $(element);
+    if ($element.attr("wt_name") === "assortment_menu.level2") {
+      let subCategoryName = $element.text();
+      helper.push(subCategoryName);
+    }
+  });
+  return helper;
+}
+
+function getParentCategory($) {
+  return $("h1").text();
+}
